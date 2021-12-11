cbc289ddaafd2976327043fc97e40f73aaca146e
Shelly Yip <shellyyip@users.noreply.github.com>
2021-12-11 13:43:37 -0500

    Call Day 2 code as IIFE

--- src/day-2/index.ts
+++ src/day-2/index.ts
@@ -1,4 +1,6 @@
 import { getValsFromFile } from "../utils";
 
-const steps = await getValsFromFile("./input");
-console.log(steps);
+(async () => {
+  const steps = await getValsFromFile("./day-2/input.txt");
+  console.log("Day 2: ", steps);
+})();
