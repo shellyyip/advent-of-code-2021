73b688a13806f5722b8deb712e31a049baf1cd62
Shelly Yip <shellyyip@users.noreply.github.com>
2021-12-18 14:58:59 -0500

    Implement getPowerConsumption, start on part 2

--- src/day-3/index.test.ts
+++ src/day-3/index.test.ts
@@ -1,4 +1,12 @@
-import { rotateArray, getMostCommonBit } from "./index";
+import { getValsFromFile } from "../utils";
+import {
+  rotateArray,
+  getCommonBit,
+  convertBinaryToInteger,
+  getRate,
+  getPowerConsumption,
+  getRating,
+} from "./index";
 
 describe("day 3", () => {
   it("can rotate an array", () => {
@@ -9,31 +17,58 @@ describe("day 3", () => {
   });
 
   it("can get the most common bit in a column", () => {
-    const actual = getMostCommonBit([
-      "0",
-      "1",
-      "1",
-      "1",
-      "1",
-      "0",
-      "0",
-      "1",
-      "1",
-      "1",
-      "0",
-      "0",
-    ]);
+    const actual = getCommonBit(
+      ["0", "1", "1", "1", "1", "0", "0", "1", "1", "1", "0", "0"],
+      "most"
+    );
     const expected = "1";
     expect(actual).toEqual(expected);
   });
 
-  it.skip("can convert binary to integer", () => {
+  it("can get the least common bit in a column", () => {
+    const actual = getCommonBit(
+      ["0", "1", "1", "1", "1", "0", "0", "1", "1", "1", "0", "0"],
+      "least"
+    );
+    const expected = "0";
+    expect(actual).toEqual(expected);
+  });
+
+  it("can convert binary to integer", () => {
     const testCases: [string, number][] = [
       ["10110", 22],
       ["01001", 9],
     ];
     testCases.forEach(([actual, expected]) => {
-      expect(actual).toEqual(expected);
+      expect(convertBinaryToInteger(actual)).toEqual(expected);
     });
+  });
+
+  it("can get the gamma rate in binary from a set of data", async () => {
+    const testData = await getValsFromFile("./day-3/test-input.txt");
+    const actual = getRate(testData, "gamma");
+    const expected = "10110";
+    expect(actual).toEqual(expected);
+  });
+
+  it("can get the correct power consumption", async () => {
+    const testData = await getValsFromFile("./day-3/test-input.txt");
+    const actual = getPowerConsumption(testData);
+    const expected = 198;
+    expect(actual).toEqual(expected);
+  });
+
+  it("can get the correct oxygen generator rating", async () => {
+    const testData = await getValsFromFile("./day-3/test-input.txt");
+    const actual = getRating(testData, "oxygen-generator");
+    const expected = 23;
+    expect(actual).toEqual(expected);
+  });
+
+  it("can get the correct CO2 scrubber rating", async () => {
+    const testData = await getValsFromFile("./day-3/test-input.txt");
+    const actual = getRating(testData, "co2-scrubber");
+    const expected = 10;
+    expect(actual).toEqual(expected);
   });
 });
--- src/day-3/index.ts
+++ src/day-3/index.ts
@@ -15,7 +15,15 @@ export const rotateArray = (arr: string[]): string[] => {
   return result;
 };
 
-export const getMostCommonBit = (col: string[]) => {
+function getInverseBit(bit) {
+  if (bit === "1") {
+    return "0";
+  } else {
+    return "1";
+  }
+}
+
+export const getCommonBit = (col: string[], type: "most" | "least"): string => {
   const counts = new Map();
   for (const el of col) {
     if (!counts.has(el)) {
@@ -25,11 +33,55 @@ export const getMostCommonBit = (col: string[]) => {
   }
 
   let mostCommonKey = undefined;
-  for (const [key, val] of counts.entries()) {
+  for (const [key, val] of Array.from(counts.entries())) {
     if (mostCommonKey === undefined || val > counts.get(mostCommonKey)) {
       mostCommonKey = key;
     }
   }
 
-  return mostCommonKey;
+  if (type === "most") {
+    return mostCommonKey;
+  } else {
+    return getInverseBit(mostCommonKey);
+  }
 };
+
+export function convertBinaryToInteger(b: string): number {
+  return parseInt(b, 2);
+}
+
+export function getRate(data: string[], type: "gamma" | "epsilon"): string {
+  const columns = rotateArray(data);
+  const result = [];
+  const commonBitType = type === "gamma" ? "most" : "least";
+
+  columns.forEach((column) => {
+    result.push(getCommonBit(column.split(""), commonBitType));
+  });
+
+  return result.join("");
+}
+
+export function getPowerConsumption(data: string[]): number {
+  const gammaRate = convertBinaryToInteger(getRate(data, "gamma"));
+  const epsilonRate = convertBinaryToInteger(getRate(data, "epsilon"));
+  return gammaRate * epsilonRate;
+}
+
+export function getRating(
+  initialData: string[],
+  type: "oxygen-generator" | "co2-scrubber"
+): number {
+  const data = [...initialData];
+  const columns = rotateArray(initialData);
+
+  // For each column, from first to fifth...
+  // Find the most common bit in this column.
+  // Filter out all nums from orig. data that do not have this bit in this column.
+
+  return convertBinaryToInteger(data[0]);
+}
+
+(async function () {
+  console.log(getPowerConsumption(await getValsFromFile("./day-3/input.txt")));
+})();
