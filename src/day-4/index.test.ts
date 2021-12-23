fedf4094ba752a27421e21d086d7b83363a178d2
Shelly Yip <shellyyip@users.noreply.github.com>
2021-12-23 18:13:22 -0500

    Complete Day 5

--- src/day-4/index.test.ts
+++ src/day-4/index.test.ts
@@ -20,27 +20,87 @@ describe("day 4", () => {
 
     it("can parse bingo boards from input", () => {
       const expected = [
-        `22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19`
-          .split(" ")
-          .filter((s) => s !== ""),
-        ` 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6`
-          .split(" ")
-          .filter((s) => s !== ""),
-        `14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7`
-          .split(" ")
-          .filter((s) => s !== ""),
+        new BingoBoard([
+          "22",
+          "13",
+          "17",
+          "11",
+          "0",
+          "8",
+          "2",
+          "23",
+          "4",
+          "24",
+          "21",
+          "9",
+          "14",
+          "16",
+          "7",
+          "6",
+          "10",
+          "3",
+          "18",
+          "5",
+          "1",
+          "12",
+          "20",
+          "15",
+          "19",
+        ]),
+        new BingoBoard([
+          "3",
+          "15",
+          "0",
+          "2",
+          "22",
+          "9",
+          "18",
+          "13",
+          "17",
+          "5",
+          "19",
+          "8",
+          "7",
+          "25",
+          "23",
+          "20",
+          "11",
+          "10",
+          "24",
+          "4",
+          "14",
+          "21",
+          "16",
+          "12",
+          "6",
+        ]),
+        new BingoBoard([
+          "14",
+          "21",
+          "17",
+          "24",
+          "4",
+          "10",
+          "16",
+          "15",
+          "9",
+          "19",
+          "18",
+          "8",
+          "23",
+          "26",
+          "20",
+          "22",
+          "11",
+          "13",
+          "6",
+          "5",
+          "2",
+          "0",
+          "12",
+          "3",
+          "7",
+        ]),
       ];
       const actual = TestBingoGame.boards;
       expect(actual).toEqual(expected);
@@ -51,11 +111,7 @@ describe("day 4", () => {
     let Board;
     beforeEach(() => {
       Board = new BingoBoard(
-        `14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7`
+        "14 21 17 24  4 10 16 15  9 19 18  8 23 26 20 22 11 13  6  5 2  0 12  3  7"
           .split(" ")
           .filter((s) => s !== "")
           .map((s) => s.trim())
@@ -64,21 +120,72 @@ describe("day 4", () => {
 
     it("can mark a spot", () => {
       Board.markBoard("14");
-      console.log(Board);
       expect(Board.markedIndices).toEqual([0]);
+      expect(Board.victoryConditions).toEqual([
+        ["21", "17", "24", "4"],
+        ["10", "16", "15", "9", "19"],
+        ["18", "8", "23", "26", "20"],
+        ["22", "11", "13", "6", "5"],
+        ["2", "0", "12", "3", "7"],
+        ["10", "18", "22", "2"],
+        ["21", "16", "8", "11", "0"],
+        ["17", "15", "23", "13", "12"],
+        ["24", "9", "26", "6", "3"],
+        ["4", "19", "20", "5", "7"],
+      ]);
     });
 
-    it("can detect a horizontal win", () => {
-      const calledNumbers = "7,4,9,5,11,17,23,2,0,14,21,24".split(",");
-      expect(Board.won).toEqual(true);
-    });
+    describe("winning", () => {
+      let WinningBoard;
+      beforeAll(() => {
+        WinningBoard = new BingoBoard(
+          "14 21 17 24 4 10 16 15  9 19 18  8 23 26 20 22 11 13 6 5 2 0 12 3 7"
+            .split(" ")
+            .filter((s) => s !== "")
+            .map((s) => s.trim())
+        );
 
-    it.skip("can return its winning turn", () => {});
-    it.skip("can sum up a winning board", () => {
-      const expected = 188;
+        const calledNumbers =
+          "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1".split(
+            ","
+          );
+
+        for (const num of calledNumbers) {
+          WinningBoard.takeTurn(num);
+        }
+      });
+
+      it("can detect a horizontal win", () => {
+        expect(WinningBoard.won).toEqual(true);
+      });
+
+      it("can return its winning turn", () => {
+        console.log(WinningBoard);
+        const expected = 12;
+        const actual = WinningBoard.calledNumbers.length;
+        expect(actual).toEqual(expected);
+      });
+
+      it("can sum up a winning board", () => {
+        const expected = 188;
+        const actual = WinningBoard.winningSum;
+        expect(actual).toEqual(expected);
+      });
+
+      it("can score a winning board", () => {
+        const expected = 4512;
+        const actual = WinningBoard.score;
+        expect(actual).toEqual(expected);
+      });
     });
-    it.skip("can score a winning board", () => {
-      const expected = 4512;
+  });
+
+  describe("BingoGame class", () => {
+    it("can identify the first winning board", () => {
+      const expected = 2;
+      TestBingoGame.run();
+      const actual = TestBingoGame.winningBoardIndices[0];
+      expect(actual).toEqual(expected);
     });
   });
 });
--- src/day-4/index.ts
+++ src/day-4/index.ts
@@ -3,7 +3,8 @@ import { readFile } from "../utils";
 class BingoGame {
   data: string;
   calledNumbers: string[];
-  boards: string[][];
+  boards: BingoBoard[];
+  winningBoardIndices: number[];
 
   constructor(data: string) {
     this.data = data;
@@ -11,17 +12,34 @@ class BingoGame {
     this.boards = this.data
       .split("\n\n")
       .slice(1)
-      .map((s) => s.split(" ").filter((s) => s !== ""));
+      .map((rawBoard) => {
+        const boardData = rawBoard
+          .split(/\s/)
+          .filter((num) => num !== "")
+          .map((num) => num.trim());
+        return new BingoBoard(boardData);
+      });
+    this.winningBoardIndices = [];
   }
 
-  run() {}
+  run() {
+    for (const num of this.calledNumbers) {
+      for (let i = 0; i < this.boards.length; i++) {
+        const Board = this.boards[i];
+        Board.takeTurn(num);
+        if (Board.won) {
+          this.winningBoardIndices.push(i);
+        }
+      }
+    }
+  }
 }
 
-function generateVictoryConditions() {
+function generateVictoryConditionsIndices(): number[][] {
   const result = [];
 
   // Generate horizontals.
-  let row = [];
+  let row: number[] = [];
   for (let i = 0; i < 25; i++) {
     if (row.length < 5) {
       row.push(i);
@@ -43,8 +61,8 @@ function generateVictoryConditions() {
   }
 
   // Generate diagonals
-  result.push([0, 6, 12, 18, 24]);
-  result.push([4, 8, 12, 16, 20]);
+  // result.push([0, 6, 12, 18, 24]);
+  // result.push([4, 8, 12, 16, 20]);
 
   return result;
 }
@@ -52,30 +70,103 @@ function generateVictoryConditions() {
 class BingoBoard {
   won: boolean;
   numbers: string[];
+  calledNumbers: string[];
   markedIndices: number[];
-  victoryConditions: number[][];
+  victoryConditions: string[][];
+  winningSum: number;
+  score: number;
 
   constructor(data: string[]) {
     this.won = false;
     this.numbers = data;
     this.markedIndices = [];
-    this.victoryConditions = generateVictoryConditions();
+    this.victoryConditions = generateVictoryConditionsIndices().map((arr) =>
+      arr.map((i) => this.numbers[i])
+    );
+    this.winningSum = 0;
+    this.score = 0;
+    this.calledNumbers = [];
   }
 
   checkForWin(): void {
     if (this.markedIndices.length >= 5) {
-      // Check if
+      // Check if any of the victory conditions have length 0.
+      // If so, set won to true.
+      for (const cond of this.victoryConditions) {
+        if (cond.length === 0) {
+          this.won = true;
+          this.sumRemainingNumbers();
+          this.calculateScore(
+            parseInt(this.calledNumbers[this.calledNumbers.length - 1])
+          );
+          return;
+        }
+      }
     }
   }
 
   markBoard(calledNum: string): void {
     // Find if this calledNum is in the array of numbers on the board.
     // If so, push this spot's index to marked indices.
+    this.calledNumbers.push(calledNum);
     const spot = this.numbers.findIndex((el) => el === calledNum);
     if (spot > -1) {
       this.markedIndices.push(spot);
+      // Delete this index from all victory conditions.
+      this.victoryConditions.forEach((cond) => {
+        const i = cond.findIndex((el) => el === calledNum);
+        if (i > -1) {
+          cond.splice(i, 1);
+        }
+      });
+    }
+  }
+
+  sumRemainingNumbers() {
+    const remainingNumbers = this.numbers.filter(
+      (n, i) => !this.markedIndices.includes(i)
+    );
+
+    this.winningSum = remainingNumbers
+      .map((n) => parseInt(n))
+      .reduce((a = 0, c) => a + c);
+  }
+
+  calculateScore(lastNum: number) {
+    this.score = this.winningSum * lastNum;
+  }
+
+  takeTurn(calledNum: string): void {
+    if (this.won === false) {
+      this.markBoard(calledNum);
+      this.checkForWin();
     }
   }
 }
+
+function findLastWinningGame(Game: BingoGame): BingoBoard | null {
+  let longest = 0;
+  let board = null;
+
+  for (const Board of Game.boards) {
+    const currLen = Board.calledNumbers.length;
+    if (currLen >= longest) {
+      longest = currLen;
+      board = Board;
+    }
+  }
+  return board;
+}
+
+(async function () {
+  const Game = new BingoGame(await readFile("./day-4/input.txt"));
+  Game.run();
+  console.log(Game);
+  console.log(
+    "first winning game: ",
+    Game.boards[Game.winningBoardIndices[0]].score
+  );
+  console.log("last winning game: ", findLastWinningGame(Game)?.score);
+})();
 
 export { BingoGame, BingoBoard };
